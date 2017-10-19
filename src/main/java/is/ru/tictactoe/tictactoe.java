c47e2121b14ba03e5a4a619753830ef767567b73
Magnus Sigurdsson <magnussigurdsson@Magnuss-MacBook-Pro.local>
2017-10-19 15:15:26 +0000

    Added gradle to project

---
+++ src/main/java/is/ru/tictactoe/tictactoe.java
@@ -0,0 +1,10 @@
+package is.ru.tictactoe;
+import org.joda.time.DateTime;
+public class tictactoe 
+{   
+	public static void main(String[] args) 
+	{        
+		System.out.println("Hello World!");    
+	}
+}
+
