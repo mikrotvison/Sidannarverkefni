b808f0d90d24ca2cb8bce7a72e5727dce669e8b2
Magnus Sigurdsson <magnussigurdsson@Magnuss-MacBook-Pro.local>
2017-10-20 11:52:57 +0000

    Initialize game board

--- src/test/java/is/ru/tictactoe/tictactoeTest.java
+++ src/test/java/is/ru/tictactoe/tictactoeTest.java
@@ -1,8 +1,22 @@
-package is.ru.stringcalculator;
+package is.ru.tictactoe;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class tictactoeTest {
+public class TictactoeTest {
+	@Test
+	public void TestInitializationBoard()
+	{
+		Tictactoe t = new Tictactoe();
+		t.InitializeBoard();
+		
+		for (int i = 0; i < t.SIZE; i++)
+                {
+                        for (int j = 0; j < t.SIZE; j++)
+                        {
+                                assertEquals(0, t.gameBoard[i][j]);
+                        }
+                }
+	}
 
 }
