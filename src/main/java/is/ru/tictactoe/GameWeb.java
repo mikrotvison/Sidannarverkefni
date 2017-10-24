8217a747cadc51628aafa4e5d8992e7b46ee3772
maggibjorn <mb14@ru.is>
2017-10-24 16:15:19 +0000

    Initial commit

--- src/main/java/is/ru/tictactoe/GameWeb.java
+++ src/main/java/is/ru/tictactoe/GameWeb.java
@@ -4,16 +4,19 @@ import java.util.*;
 public class GameWeb {
 
     public static void main(String[] args) {
+        
         port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello Heroku World");
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
+    private static int getHerokuAssignedPort() {
+        final ProcessBuilder processBuilder = new ProcessBuilder();
+
         if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+          return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+
+        return 4567; // default Spark port
     }
 
 }
