d7320f88ac2e72fc622f6e3f52e9181cd3319195
maggibjorn <mb14@ru.is>
2017-10-24 13:24:22 +0000

    Pushing to heroku repo

---
+++ src/main/java/is/ru/tictactoe/GameWeb.java
@@ -0,0 +1,19 @@
+package is.ru.tictactoe;
+import static spark.Spark.*;
+import java.util.*;
+public class GameWeb {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
