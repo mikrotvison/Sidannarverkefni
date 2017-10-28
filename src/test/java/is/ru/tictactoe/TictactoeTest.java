a6b4517f801a1a33736b769c64b1bef5f50d36cc
Íris Björk <iriss14@ru.is>
2017-10-28 17:31:09 +0000

    unit test

--- src/test/java/is/ru/tictactoe/TictactoeTest.java
+++ src/test/java/is/ru/tictactoe/TictactoeTest.java
@@ -5,125 +5,125 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 public class TictactoeTest
 {
-        // Test game board init
-        @Test
-        public void TestBoardInit()
+    // Test game board init
+    @Test
+    public void TestBoardInit()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeBoard();
+        
+        for (int i = 0; i < t.SIZE; i++)
         {
-                Tictactoe t = new Tictactoe();
-                t.InitializeBoard();
-
-                for (int i = 0; i < t.SIZE; i++)
-                {
-                        for (int j = 0; j < t.SIZE; j++)
-                        {
-                                assertEquals(t.STATUS_EMPTY, t.gameBoard[i][j]);
-                        }
-                }
+            for (int j = 0; j < t.SIZE; j++)
+            {
+                assertEquals(t.STATUS_EMPTY, t.gameBoard[i][j]);
+            }
         }
-	
-	// Test status of current player when game is initialized
-	@Test
-	public void TestGameInit()
-	{
-		Tictactoe t = new Tictactoe();
-		t.InitializeGame();
-		assertEquals(t.PLAYER_X, t.currPlayer);
-	}
-	
-	// Test player move on game board
-	@Test
-	public void TestPlayerXMoveToThirdSlot()
-	{
-		Tictactoe t = new Tictactoe();
-		t.InitializeGame();
-		t.PlayerMove(3);			// Player_X moves to slot (0,2)			
-		assertEquals(t.PLAYER_X, t.gameBoard[0][2]);
-	}
-
-	// Test another player move on game board
-        @Test
-        public void TestPlayerOMoveToNinthSlot()
+    }
+    
+    // Test status of current player when game is initialized
+    @Test
+    public void TestGameInit()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        assertEquals(t.PLAYER_X, t.currPlayer);
+    }
+    
+    // Test player move on game board
+    @Test
+    public void TestPlayerXMoveToThirdSlot()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        t.PlayerMove(3);			// Player_X moves to slot (0,2)
+        assertEquals(t.PLAYER_X, t.gameBoard[0][2]);
+    }
+    
+    // Test another player move on game board
+    @Test
+    public void TestPlayerOMoveToNinthSlot()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        t.PlayerMove(3);                        // Player_X moves to slot (0,2)
+        t.PlayerMove(9);			// PLAYER_O moves to slot (2,2)
+        assertEquals(t.PLAYER_O, t.gameBoard[2][2]);
+    }
+    
+    // Test another player move on game board
+    @Test
+    public void TestPlayerXMoveToFifthSlot()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        t.PlayerMove(5);                        // Player_X moves to slot (1,1)
+        assertEquals(t.PLAYER_X, t.gameBoard[1][1]);
+    }
+    
+    // Test invalid player move
+    @Test
+    public void TestInvalidPlayerMove()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        t.PlayerMove(13);                       // Invalid move
+        for (int i = 0; i < t.SIZE; i++)
         {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                t.PlayerMove(3);                        // Player_X moves to slot (0,2)
-		t.PlayerMove(9);			// PLAYER_O moves to slot (2,2)
-                assertEquals(t.PLAYER_O, t.gameBoard[2][2]);
+            for (int j = 0; j < t.SIZE; j++)
+            {
+                assertEquals(t.STATUS_EMPTY, t.gameBoard[i][j]);
+            }
         }
-
-        // Test another player move on game board
-        @Test
-        public void TestPlayerXMoveToFifthSlot()
-        {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                t.PlayerMove(5);                        // Player_X moves to slot (1,1)
-                assertEquals(t.PLAYER_X, t.gameBoard[1][1]);
-        }
-
-        // Test invalid player move
-        @Test
-        public void TestInvalidPlayerMove()
-        {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                t.PlayerMove(13);                       // Invalid move
-		for (int i = 0; i < t.SIZE; i++)
-		{
-			for (int j = 0; j < t.SIZE; j++)
-			{
-				assertEquals(t.STATUS_EMPTY, t.gameBoard[i][j]); 
-			}
-		}
-                
-        }
-	
-	// Test player move if game is in status DRAW
-        @Test
-        public void TestPlayerMoveWhenDraw()
-        {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                					// Create when game ends in a draw
-		t.PlayerMove(2);
-		t.PlayerMove(1);
-                t.PlayerMove(5);
-                t.PlayerMove(3);
-                t.PlayerMove(6);
-                t.PlayerMove(4);
-                t.PlayerMove(7);
-                t.PlayerMove(8);
-		t.PlayerMove(9);         
-                assertEquals(true, t.IsDraw());		// Game ended in a draw, should return true
-        }
-	
-        // Test draw function when game is still in playing mode
-        @Test
-        public void TestPlayerMoveWhenNotDraw()
-        {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                                                        // Create a game that is not in draw status
-                t.PlayerMove(2);
-                t.PlayerMove(1);
- 		assertEquals(false, t.IsDraw());	               
-        }
-	
-        // Test won function when player x has won the game 
-        @Test
-        public void TestPlayerXHasWon()
-        {
-                Tictactoe t = new Tictactoe();
-                t.InitializeGame();
-                                                        // Create a game that player x wins
-                t.PlayerMove(1);
-                t.PlayerMove(2);
-                t.PlayerMove(4);
-                t.PlayerMove(3);
-		String player = t.PlayerMove(7);	// PlayerMove returns player who did the move and swaps players
-                assertEquals(true, t.GameWon(player, 7));
-        }
-	
+        
+    }
+    
+    // Test player move if game is in status DRAW
+    @Test
+    public void TestPlayerMoveWhenDraw()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        // Create when game ends in a draw
+        t.PlayerMove(2);
+        t.PlayerMove(1);
+        t.PlayerMove(5);
+        t.PlayerMove(3);
+        t.PlayerMove(6);
+        t.PlayerMove(4);
+        t.PlayerMove(7);
+        t.PlayerMove(8);
+        t.PlayerMove(9);
+        assertEquals(true, t.IsDraw());		// Game ended in a draw, should return true
+    }
+    
+    // Test draw function when game is still in playing mode
+    @Test
+    public void TestPlayerMoveWhenNotDraw()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        // Create a game that is not in draw status
+        t.PlayerMove(2);
+        t.PlayerMove(1);
+        assertEquals(false, t.IsDraw());
+    }
+    
+    // Test won function when player x has won the game
+    @Test
+    public void TestPlayerXHasWon()
+    {
+        Tictactoe t = new Tictactoe();
+        t.InitializeGame();
+        // Create a game that player x wins
+        t.PlayerMove(1);
+        t.PlayerMove(2);
+        t.PlayerMove(4);
+        t.PlayerMove(3);
+        String player = t.PlayerMove(7);	// PlayerMove returns player who did the move and swaps players
+        assertEquals(true, t.GameWon(player, 7));
+    }
+    
     // Test won function when player O has won the game
     @Test
     public void TestPlayerOHasWon()
@@ -223,8 +223,6 @@ public class TictactoeTest
         assertEquals(t.PLAYER_O, t.currPlayer);
         
     }
+    
 }
-    
-    
-    
-    
+
